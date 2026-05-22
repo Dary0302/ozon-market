8d3c5d0d5e8877e67ce823602b6dbfd7e84780b1
kurlyushonok <history05mikano@gmail.com>
2026-05-22 21:39:48 +0500

    вынесла регистрацию инфраструктуры в отдельный метод

--- src/OzonMarketOrder/OzonMarketOrder.Api/Startup.cs
+++ src/OzonMarketOrder/OzonMarketOrder.Api/Startup.cs
@@ -1,29 +1,19 @@
-﻿using Core.Common;
-using Dapper;
-using Core.Common.DbHelpers;
+﻿using OzonMarketOrder.Infrastructure;
 using OzonMarketOrder.Api.Extensions;
 using OzonMarketOrder.Application;
-using OzonMarketOrder.Application.Interfaces;
-using OzonMarketOrder.Infrastructure.Implementations;
 
 namespace OzonMarketOrder.Api;
 
-public class Startup(IConfiguration configuration, IHostBuilder hostBuilder)
+public class Startup(IConfiguration configuration)
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        DefaultTypeMap.MatchNamesWithUnderscores = true;
-        
         services.AddControllers();
 
         services.AddApplicationServices()
             .AddOpenApi();
-        
-        var connectionString = configuration.GetConnectionString("DefaultConnection")
-            ?? throw new NullReferenceException("No database connection string found.");;
-        
-        services.AddSingleton<IPostgresConnectionFactory>(new PostgresConnectionFactory(connectionString));
-        services.AddSingleton<IOrderRepository, OrderRepository>();
+
+        services.AddInfrastructureServices(configuration);
     }
 
     public void Configure(IApplicationBuilder app)
