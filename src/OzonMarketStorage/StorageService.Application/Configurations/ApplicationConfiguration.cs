6af48144358179e4fc0baef18c9c926cce2e649a
Laert <nlovnas@mail.ru>
2026-05-31 20:13:11 +0500

    Зарегистрированы DI

--- src/OzonMarketStorage/StorageService.Application/Configurations/ApplicationConfiguration.cs
+++ src/OzonMarketStorage/StorageService.Application/Configurations/ApplicationConfiguration.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using StorageService.Application.Interfaces.Services;
 
 namespace StorageService.Application.Configurations;
 
@@ -6,10 +7,12 @@ public static class ApplicationConfiguration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        // services
-            //TODO: Добавить реализации всех хендлеров и сервисов
-            // .AddScoped<IAnimalService, AnimalService>()
-            // .AddScoped<IEntityChangeHandler, HuntChangeHandler>()
+        services
+            .AddScoped<IStorageService, Implementations.StorageService>()
+            .AddScoped<IStoragePointService, Implementations.StoragePointService>()
+            .AddScoped<IPvzService, Implementations.PvzService>()
+            .AddScoped<IPvzPointService, Implementations.PvzPointService>()
+            .AddScoped<IStoredProductService, Implementations.StoredProductService>();
         
         return services;
     }
