f83d44200cd993484102a9a36731e475869d8df5
kurlyushonok <history05mikano@gmail.com>
2026-06-03 18:16:40 +0500

    добавила валидацию

---
+++ src/OrderService/OrderService.Api/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using OrderService.Domain;
+using FluentValidation;
+using OrderService.Api.Dto;
+
+namespace OrderService.Api.Validators;
+
+public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequestDto>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(dto => dto.PvzId)
+            .NotEmpty()
+            .WithMessage("Id пункта выдачи заказов не может быть пустым");
+
+        RuleFor(dto => dto.ClientAmount)
+            .GreaterThan(0)
+            .WithMessage("Сумма заказа должна быть больше нуля");
+
+        RuleFor(dto => dto.Products)
+            .NotEmpty()
+            .WithMessage("Список позиций не может быть пустым");
+
+        RuleForEach(dto => dto.Products)
+            .ChildRules(item =>
+            {
+                item.RuleFor(product => product.ProductId)
+                    .NotEmpty()
+                    .WithMessage("Id товара не может быть пустым");
+
+                item.RuleFor(product => product.Quantity)
+                    .GreaterThan(0)
+                    .WithMessage("Количество товаров должно быть больше 0");
+            });
+    }
+}
