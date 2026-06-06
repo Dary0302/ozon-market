e40acd6da8a52e2dfaab7135608c21d198f71a6f
Dary <Leontyev0302@mail.ru>
2026-06-06 20:16:31 +0500

    Прокинул CancellationToken в тесты

--- src/ProductService/ProductService.Tests/IntegrationTests/ProductRepositoryTests.cs
+++ src/ProductService/ProductService.Tests/IntegrationTests/ProductRepositoryTests.cs
@@ -39,9 +39,9 @@ public class ProductRepositoryTests
             ProductType.Table,
             Guid.NewGuid());
         
-        await repository.Add(product);
+        await repository.Add(product, CancellationToken.None);
 
-        var saved = await repository.Get(product.Id);
+        var saved = await repository.Get(product.Id, CancellationToken.None);
         
         saved.Should().NotBeNull();
         saved.Id.Should().Be(product.Id);
@@ -50,7 +50,7 @@ public class ProductRepositoryTests
     [Test]
     public async Task Get_ShouldReturnNull_WhenProductDoesNotExist()
     {
-        var result = await repository.Get(Guid.NewGuid());
+        var result = await repository.Get(Guid.NewGuid(), CancellationToken.None);
 
         result.Should().BeNull();
     }
@@ -64,11 +64,11 @@ public class ProductRepositoryTests
             ProductType.Phone,
             Guid.NewGuid());
 
-        await repository.Add(product);
+        await repository.Add(product, CancellationToken.None);
 
-        await repository.Delete(product.Id);
+        await repository.Delete(product.Id, CancellationToken.None);
 
-        var result = await repository.Get(product.Id);
+        var result = await repository.Get(product.Id, CancellationToken.None);
 
         result.Should().BeNull();
     }
@@ -76,7 +76,7 @@ public class ProductRepositoryTests
     [Test]
     public async Task Delete_ShouldNotThrow_WhenProductDoesNotExist()
     {
-        var action = async () => await repository.Delete(Guid.NewGuid());
+        var action = async () => await repository.Delete(Guid.NewGuid(), CancellationToken.None);
 
         await action.Should().NotThrowAsync();
     }
@@ -90,7 +90,7 @@ public class ProductRepositoryTests
             ProductType.Table,
             Guid.NewGuid());
 
-        await repository.Add(product);
+        await repository.Add(product, CancellationToken.None);
 
         var updatedProduct = new Product(
             "Laptop",
@@ -98,12 +98,12 @@ public class ProductRepositoryTests
             ProductType.Bed,
             Guid.NewGuid());
 
-        await repository.Update(product.Id, updatedProduct);
+        await repository.Update(product.Id, updatedProduct, CancellationToken.None);
 
-        var result = await repository.Get(product.Id);
+        var result = await repository.Get(product.Id, CancellationToken.None);
 
         result.Should().NotBeNull();
-        result!.Id.Should().Be(product.Id);
+        result.Id.Should().Be(product.Id);
         result.Name.Should().Be(updatedProduct.Name);
         result.Description.Should().Be(updatedProduct.Description);
         result.Type.Should().Be(updatedProduct.Type);
@@ -121,9 +121,9 @@ public class ProductRepositoryTests
             ProductType.Toy,
             Guid.NewGuid());
 
-        await repository.Update(id, product);
+        await repository.Update(id, product, CancellationToken.None);
 
-        var result = await repository.Get(id);
+        var result = await repository.Get(id, CancellationToken.None);
 
         result.Should().BeNull();
     }
