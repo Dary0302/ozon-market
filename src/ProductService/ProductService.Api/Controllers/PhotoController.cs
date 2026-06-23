6e85553867e8b9899450e9dd2b7fbc4ed311159b
Dary <Leontyev0302@mail.ru>
2026-06-23 12:33:50 +0500

    Фикс работы с s3

--- src/ProductService/ProductService.Api/Controllers/PhotoController.cs
+++ src/ProductService/ProductService.Api/Controllers/PhotoController.cs
@@ -7,19 +7,26 @@ namespace ProductService.Api.Controllers;
 
 [ApiController]
 [Route("api/photos")]
-public class PhotoController(IPhotoService service) : ControllerBase
+public class PhotoController(IPhotoService service, IProductManagementService productService) : ControllerBase
 {
     /// <summary>
     /// Получение ссылки на фото по id
     /// </summary>
-    /// <param name="photoId">Id фото</param>
+    /// <param name="productId">Id фото</param>
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [HttpGet("link/{photo-id:guid}")]
+    [HttpGet("link/{product-id:guid}")]
     public async Task<ActionResult<GetPhotoLinkDto>> Get(
-        [FromRoute(Name = "photo-id")] Guid photoId,
+        [FromRoute(Name = "product-id")] Guid productId,
         CancellationToken cancellationToken)
     {
+        var photoIdResult = await productService.GetProduct(productId, cancellationToken);
+        if (photoIdResult.Value.PhotoId is null)
+        {
+            return NotFound();
+        }
+
+        var photoId = photoIdResult.Value.PhotoId.Value;
         var photoLinkResult = await service.GetPhotoLinkByIdAsync(photoId, cancellationToken);
         return photoLinkResult.ToActionResult();
     }
