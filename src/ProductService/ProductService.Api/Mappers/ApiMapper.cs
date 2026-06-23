cc149bcdaf8d519488045c1390b7822807f637e7
Dary <Leontyev0302@mail.ru>
2026-06-24 00:12:56 +0500

    Перевёл типы на русский

--- src/ProductService/ProductService.Api/Mappers/ApiMapper.cs
+++ src/ProductService/ProductService.Api/Mappers/ApiMapper.cs
@@ -17,8 +17,26 @@ public static class ApiMapper
             Id = product.Id,
             Name = product.Name,
             Description = product.Description,
-            Type = product.Type.ToString(),
+            Type = product.Type.ToHttp(),
             PhotoId = product.PhotoId
+        };
+    }
+
+    public static string ToHttp(this ProductType type)
+    {
+        return type switch
+        {
+            ProductType.Undefined => "Undefined",
+            ProductType.Phone => "Телефон",
+            ProductType.Tablet => "Планшет",
+            ProductType.Headphones => "Наушники",
+            ProductType.Laptop => "Ноутбук",
+            ProductType.TV => "ТВ",
+            ProductType.Smartwatch => "Умные часы",
+            ProductType.Console => "Консоль",
+            ProductType.Camera => "Камера",
+            ProductType.Monitor => "Монитор",
+            var _ => type.ToString()
         };
     }
 }
